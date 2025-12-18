a9290655469b9b8a5a2d4b8473b9d2b35f625d08
SamWieHTWG <wiertzsamuel@gmail.com>
2025-12-18 16:25:47 +0100

    update

---
+++ _app/immutable/entry/start.DkpXrnJ4.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CVH89Ods.js";import{v as t}from"../chunks/Db0jUvVj.js";export{t as load_css,r as start};
--- _app/immutable/nodes/5.CBGU8h0G.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/CWj6FrbW.js";import"../chunks/BfN-51NE.js";import{f as o,h as r,a as n,$ as p,n as s}from"../chunks/BuFjotJ2.js";import"../chunks/BR9XSesW.js";import"../chunks/DcyeeV-y.js";var e=o("<h1>Contact Us</h1> <p>This is the contact page for statiqs.</p>",1);function d(t){var a=e();r(i=>{p.title="Contact - statiqs"}),s(2),n(t,a)}export{d as component};
