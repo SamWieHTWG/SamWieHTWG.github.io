8bafc9de041d1a1542debbc816e02c8837b280f4
SamWieHTWG <wiertzsamuel@gmail.com>
2025-12-19 15:02:01 +0100

    update to v1.4.4

--- _app/immutable/entry/start.C5MthA5o.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BnRYaebf.js";import{y as t}from"../chunks/C4sS2T11.js";export{t as load_css,r as start};
---
+++ _app/immutable/entry/start.CO06vvwL.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/CrYEm_2K.js";import{y as t}from"../chunks/CymU5GQd.js";export{t as load_css,r as start};
